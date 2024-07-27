15fa9f3f2b9c73d00adcece75afaf5740ad357df
Semalmahajan <64732701+Semalmahajan@users.noreply.github.com>
2024-07-28 00:24:35 +0530

    updated redirect to login

--- frontend/src/components/RedirectToLogin.js
+++ frontend/src/components/RedirectToLogin.js
@@ -10,21 +10,20 @@ const RedirectToLogin = () => {
 
   return (
     <div
-      className="flex justify-center flex-col items-center bg-slate-900"
-      style={{ height: "37rem" }}
+      className="flex justify-center items-center bg-slate-900"
+      style={{ height: "100vh" }}
     >
       <div
-        className="flex justify-center flex-col items-center w-4/12 border border-red-600 bg-black "
-        style={{ height: "250px" }}
+        className="flex justify-center flex-col items-center w-full sm:w-10/12 md:w-8/12 lg:w-6/12 xl:w-4/12 border border-red-600 bg-black p-4"
       >
-        <h1 className="font-semibold text-white">
+        <h1 className="font-semibold text-white text-center mb-4">
           Please Login first to proceed ahead
         </h1>
         <button
           onClick={handleClick}
-          className="px-3 py-1 m-2 rounded-full text-white bg-red-600 hover:bg-red-700"
+          className="px-4 py-2 rounded-full text-white bg-red-600 hover:bg-red-700"
         >
-          CLick here to Login
+          Click here to Login
         </button>
       </div>
     </div>
